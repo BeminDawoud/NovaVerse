c7a41fef800858c1835c20c7d8d25d6297d4b758
Bemin Dawoud <bemin_dawoud@yahoo.com>
2024-06-27 20:23:07 +0300

    home template complete

--- static/home.js
+++ static/home.js
@@ -1,7 +1,14 @@
 
-// Side-Bar
-const menuItems = document.querySelectorAll(".menu-item");
 
+const menuItems = document.querySelectorAll(".menu-item");
+const theme = document.querySelector('#theme');
+const themeModel = document.querySelector('.customized-theme');
+const fontSizes = document.querySelectorAll('.choose-size span');
+const colorPalette = document.querySelectorAll('.choose-color span');
+const root = document.querySelector(':root')
+
+
+// *******************Side-Bar*************************
 // remove class active from all items
 const changeActiveItem = () => {
 	menuItems.forEach(item => {
@@ -20,5 +27,89 @@ menuItems.forEach(item => {
 			document.querySelector('.notifications-popup').style.display = 'block';
 			document.querySelector('#notifications .notifications-count').style.display = 'none';
 		}
+	})
+})
+
+
+// **************** theme customization ********************
+// open and close theme card
+const openThemeModel = () => {
+	themeModel.style.display='grid';
+}
+const closeThemeModel = (e) => {
+	if(e.target.classList.contains('customized-theme')){
+		themeModel.style.display='none';
+	}
+}
+theme.addEventListener('click', openThemeModel);
+themeModel.addEventListener('click', closeThemeModel);
+
+
+
+// ************************ change font ********************
+// remove active class
+const removeFontActiveClass = () => {
+	fontSizes.forEach(size => {
+		size.classList.remove('active');
+	})
+}
+
+
+fontSizes.forEach(size => {
+	size.addEventListener('click', () => {
+		let fontSize;
+		removeFontActiveClass();
+		size.classList.toggle('active');
+		if(size.classList.contains('font-size-1')){
+			fontSize = '10px';
+			root.style.setProperty('--sticky-top-left', '5.4rem');
+			root.style.setProperty('--sticky-top-right', '5.4rem');
+		} else if(size.classList.contains('font-size-2')){
+			fontSize = '13px';
+			root.style.setProperty('--sticky-top-left', '5.4rem');
+			root.style.setProperty('--sticky-top-right', '-7rem');
+		} else if(size.classList.contains('font-size-3')){
+			fontSize = '16px';
+			root.style.setProperty('--sticky-top-left', '-2rem');
+			root.style.setProperty('--sticky-top-right', '-17rem');
+		} else if(size.classList.contains('font-size-4')){
+			fontSize = '19px';
+			root.style.setProperty('--sticky-top-left', '-5rem');
+			root.style.setProperty('--sticky-top-right', '-25rem');
+		} else if(size.classList.contains('font-size-5')){
+			fontSize = '22px';
+			root.style.setProperty('--sticky-top-left', '-10rem');
+			root.style.setProperty('--sticky-top-right', '-33rem');
+		}
+		document.querySelector('html').style.fontSize = fontSize;
+	})
+})
+
+
+// ************************ change color ********************
+const removeActiveColorClass = () => {
+	colorPalette.forEach( color => {
+		color.classList.remove('active');
+	})
+}
+
+colorPalette.forEach(color => {
+	color.addEventListener('click', () => {
+		let primaryHue;
+		removeActiveColorClass();
+		color.classList.toggle('active');
+		if(color.classList.contains('color-1')){
+			primaryHue = 210;
+		} else if(color.classList.contains('color-2')){
+			primaryHue = 310;
+		} else if(color.classList.contains('color-3')){
+			primaryHue = 620;
+		} else if(color.classList.contains('color-4')){
+			primaryHue = 840;
+		} else if(color.classList.contains('color-5')){
+			primaryHue = 6500;
+		}
+
+		root.style.setProperty('--primary-color-hue', primaryHue)
 	})
 })
