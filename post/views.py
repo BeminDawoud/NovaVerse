380d205843a757067a7f9b38518a80c1e520e316
Bemin Dawoud <bemin_dawoud@yahoo.com>
2024-07-03 03:12:15 +0300

    create Post Models

--- post/views.py
+++ post/views.py
@@ -1,10 +1,21 @@
 from django.shortcuts import render
+from post.models import Tag, Stream, Follow, Post
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
 
 def home(request):
-    return render(request, "home.html")
+    user = request.user
+    posts = Stream.objects.filter(user=user)
+    group_ids = []
+    for post in posts:
+        group_ids.append(post.post_id)
+    post_items = Post.objects.filter(id__in=group_ids).all().order_by("-posted")
+    context = {
+        "post_items": post_items,
+    }
+    return render(request, "home.html", context)
 
 
 def messages(request):
