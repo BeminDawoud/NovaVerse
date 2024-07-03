95f8260a45f2b338c7823736698a0c69475695be
Bemin Dawoud <bemin_dawoud@yahoo.com>
2024-07-03 06:05:37 +0300

    Finish create post

--- post/forms.py
+++ post/forms.py
@@ -3,15 +3,20 @@ from post.models import Post
 
 
 class newPostForm(forms.ModelForm):
-    picture = forms.ImageField(required=True)
-    caption = forms.CharField(
-        widget=forms.TextInput(
-            attrs={"class": "django-input", "placeholder": "Caption"}
+    picture = forms.ImageField(
+        required=True,
+        widget=forms.ClearableFileInput(
+            attrs={
+                "class": "input-group",
+            }
         ),
+    )
+    caption = forms.CharField(
+        widget=forms.TextInput(attrs={"class": "input-group"}),
         required=True,
     )
     tags = forms.CharField(
-        widget=forms.TextInput(attrs={"class": "django-input", "placeholder": "Tag"}),
+        widget=forms.TextInput(attrs={"class": "input-group"}),
         required=True,
     )
 
