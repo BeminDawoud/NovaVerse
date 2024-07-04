f98e4034aecdc359de9e0d21d2a1f9fc25787b2c
Bemin Dawoud <bemin_dawoud@yahoo.com>
2024-07-04 03:32:00 +0300

    Update profile satus count

--- userauth/views.py
+++ userauth/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.models import User
 from django.urls import resolve
-from post.models import Post
+from post.models import Post, Follow
 from userauth.models import Profile
 from django.core.paginator import Paginator
 
@@ -14,10 +14,14 @@ def userProfile(request, username):
 
     # profile stats
     post_count = Post.objects.filter(user=user).count()
+    following_count = Follow.objects.filter(follower=user).count()
+    followers_count = Follow.objects.filter(following=user).count()
 
     context = {
         "posts": posts,
         "profile": profile,
         "post_count": post_count,
+        "following_count": following_count,
+        "followers_count": followers_count,
     }
     return render(request, "profile.html", context)
