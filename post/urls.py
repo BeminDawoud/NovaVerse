7a21a7d8236e582d8097c61dcf8ca137e9ac34e1
Bemin Dawoud <bemin_dawoud@yahoo.com>
2024-07-05 01:33:16 +0300

    Fix Post detail page

--- post/urls.py
+++ post/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from post import views
 
 urlpatterns = [
-    path("index", views.home, name="home"),
+    path("home", views.home, name="home"),
     path("newpost", views.newPost, name="new-post"),
     path("post/<uuid:post_id>", views.postDetail, name="post-detail"),
     path("<uuid:post_id>/like", views.like, name="like"),
